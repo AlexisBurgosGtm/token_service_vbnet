51c2d8bb81880a1141d3db8d6620c462afbb6c9c
Alexis Burgos (gtm) <34293632+AlexisBurgosGtm@users.noreply.github.com>
2025-11-04 14:58:34 -0600

    inicial, solo generacion random de claves

---
+++ token_service/token.Designer.vb
@@ -0,0 +1,198 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class token
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(token))
+        Me.TimerToken = New System.Windows.Forms.Timer(Me.components)
+        Me.LB_CLAVE_1 = New System.Windows.Forms.Label()
+        Me.LB_CLAVE_2 = New System.Windows.Forms.Label()
+        Me.LB_CLAVE_3 = New System.Windows.Forms.Label()
+        Me.LB_CLAVE_4 = New System.Windows.Forms.Label()
+        Me.LB_CLAVE_5 = New System.Windows.Forms.Label()
+        Me.LB_CLAVE_6 = New System.Windows.Forms.Label()
+        Me.LB_CLAVE_7 = New System.Windows.Forms.Label()
+        Me.LB_CLAVE_8 = New System.Windows.Forms.Label()
+        Me.LB_CLAVE_9 = New System.Windows.Forms.Label()
+        Me.LB_CLAVE_10 = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.lbTimer = New System.Windows.Forms.Label()
+        Me.TimerConteo = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'TimerToken
+        '
+        Me.TimerToken.Interval = 60000
+        '
+        'LB_CLAVE_1
+        '
+        Me.LB_CLAVE_1.AutoSize = True
+        Me.LB_CLAVE_1.Location = New System.Drawing.Point(229, 30)
+        Me.LB_CLAVE_1.Name = "LB_CLAVE_1"
+        Me.LB_CLAVE_1.Size = New System.Drawing.Size(72, 13)
+        Me.LB_CLAVE_1.TabIndex = 0
+        Me.LB_CLAVE_1.Text = "LB_CLAVE_1"
+        '
+        'LB_CLAVE_2
+        '
+        Me.LB_CLAVE_2.AutoSize = True
+        Me.LB_CLAVE_2.Location = New System.Drawing.Point(229, 63)
+        Me.LB_CLAVE_2.Name = "LB_CLAVE_2"
+        Me.LB_CLAVE_2.Size = New System.Drawing.Size(72, 13)
+        Me.LB_CLAVE_2.TabIndex = 1
+        Me.LB_CLAVE_2.Text = "LB_CLAVE_2"
+        '
+        'LB_CLAVE_3
+        '
+        Me.LB_CLAVE_3.AutoSize = True
+        Me.LB_CLAVE_3.Location = New System.Drawing.Point(229, 102)
+        Me.LB_CLAVE_3.Name = "LB_CLAVE_3"
+        Me.LB_CLAVE_3.Size = New System.Drawing.Size(72, 13)
+        Me.LB_CLAVE_3.TabIndex = 2
+        Me.LB_CLAVE_3.Text = "LB_CLAVE_3"
+        '
+        'LB_CLAVE_4
+        '
+        Me.LB_CLAVE_4.AutoSize = True
+        Me.LB_CLAVE_4.Location = New System.Drawing.Point(229, 139)
+        Me.LB_CLAVE_4.Name = "LB_CLAVE_4"
+        Me.LB_CLAVE_4.Size = New System.Drawing.Size(72, 13)
+        Me.LB_CLAVE_4.TabIndex = 3
+        Me.LB_CLAVE_4.Text = "LB_CLAVE_4"
+        '
+        'LB_CLAVE_5
+        '
+        Me.LB_CLAVE_5.AutoSize = True
+        Me.LB_CLAVE_5.Location = New System.Drawing.Point(229, 175)
+        Me.LB_CLAVE_5.Name = "LB_CLAVE_5"
+        Me.LB_CLAVE_5.Size = New System.Drawing.Size(72, 13)
+        Me.LB_CLAVE_5.TabIndex = 4
+        Me.LB_CLAVE_5.Text = "LB_CLAVE_5"
+        '
+        'LB_CLAVE_6
+        '
+        Me.LB_CLAVE_6.AutoSize = True
+        Me.LB_CLAVE_6.Location = New System.Drawing.Point(229, 209)
+        Me.LB_CLAVE_6.Name = "LB_CLAVE_6"
+        Me.LB_CLAVE_6.Size = New System.Drawing.Size(72, 13)
+        Me.LB_CLAVE_6.TabIndex = 5
+        Me.LB_CLAVE_6.Text = "LB_CLAVE_6"
+        '
+        'LB_CLAVE_7
+        '
+        Me.LB_CLAVE_7.AutoSize = True
+        Me.LB_CLAVE_7.Location = New System.Drawing.Point(229, 243)
+        Me.LB_CLAVE_7.Name = "LB_CLAVE_7"
+        Me.LB_CLAVE_7.Size = New System.Drawing.Size(72, 13)
+        Me.LB_CLAVE_7.TabIndex = 6
+        Me.LB_CLAVE_7.Text = "LB_CLAVE_7"
+        '
+        'LB_CLAVE_8
+        '
+        Me.LB_CLAVE_8.AutoSize = True
+        Me.LB_CLAVE_8.Location = New System.Drawing.Point(229, 276)
+        Me.LB_CLAVE_8.Name = "LB_CLAVE_8"
+        Me.LB_CLAVE_8.Size = New System.Drawing.Size(72, 13)
+        Me.LB_CLAVE_8.TabIndex = 7
+        Me.LB_CLAVE_8.Text = "LB_CLAVE_8"
+        '
+        'LB_CLAVE_9
+        '
+        Me.LB_CLAVE_9.AutoSize = True
+        Me.LB_CLAVE_9.Location = New System.Drawing.Point(229, 314)
+        Me.LB_CLAVE_9.Name = "LB_CLAVE_9"
+        Me.LB_CLAVE_9.Size = New System.Drawing.Size(72, 13)
+        Me.LB_CLAVE_9.TabIndex = 8
+        Me.LB_CLAVE_9.Text = "LB_CLAVE_9"
+        '
+        'LB_CLAVE_10
+        '
+        Me.LB_CLAVE_10.AutoSize = True
+        Me.LB_CLAVE_10.Location = New System.Drawing.Point(229, 348)
+        Me.LB_CLAVE_10.Name = "LB_CLAVE_10"
+        Me.LB_CLAVE_10.Size = New System.Drawing.Size(78, 13)
+        Me.LB_CLAVE_10.TabIndex = 9
+        Me.LB_CLAVE_10.Text = "LB_CLAVE_10"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Location = New System.Drawing.Point(515, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(303, 387)
+        Me.GroupBox1.TabIndex = 10
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Parametros"
+        '
+        'lbTimer
+        '
+        Me.lbTimer.AutoSize = True
+        Me.lbTimer.Location = New System.Drawing.Point(384, 400)
+        Me.lbTimer.Name = "lbTimer"
+        Me.lbTimer.Size = New System.Drawing.Size(13, 13)
+        Me.lbTimer.TabIndex = 11
+        Me.lbTimer.Text = "0"
+        '
+        'TimerConteo
+        '
+        Me.TimerConteo.Interval = 1000
+        '
+        'token
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(852, 422)
+        Me.Controls.Add(Me.lbTimer)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.LB_CLAVE_10)
+        Me.Controls.Add(Me.LB_CLAVE_9)
+        Me.Controls.Add(Me.LB_CLAVE_8)
+        Me.Controls.Add(Me.LB_CLAVE_7)
+        Me.Controls.Add(Me.LB_CLAVE_6)
+        Me.Controls.Add(Me.LB_CLAVE_5)
+        Me.Controls.Add(Me.LB_CLAVE_4)
+        Me.Controls.Add(Me.LB_CLAVE_3)
+        Me.Controls.Add(Me.LB_CLAVE_2)
+        Me.Controls.Add(Me.LB_CLAVE_1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "token"
+        Me.Text = "token"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TimerToken As Timer
+    Friend WithEvents LB_CLAVE_1 As Label
+    Friend WithEvents LB_CLAVE_2 As Label
+    Friend WithEvents LB_CLAVE_3 As Label
+    Friend WithEvents LB_CLAVE_4 As Label
+    Friend WithEvents LB_CLAVE_5 As Label
+    Friend WithEvents LB_CLAVE_6 As Label
+    Friend WithEvents LB_CLAVE_7 As Label
+    Friend WithEvents LB_CLAVE_8 As Label
+    Friend WithEvents LB_CLAVE_9 As Label
+    Friend WithEvents LB_CLAVE_10 As Label
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents lbTimer As Label
+    Friend WithEvents TimerConteo As Timer
+End Class
---
+++ token_service/token.vb
@@ -0,0 +1,90 @@
+﻿
+Public Class token
+
+    Private Sub token_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Call cambiar_claves()
+
+        TimerToken.Interval = 60000
+        TimerToken.Start()
+        TimerConteo.Start()
+
+    End Sub
+
+
+
+    Private Sub TimerToken_Tick(sender As Object, e As EventArgs) Handles TimerToken.Tick
+
+        Call cambiar_claves()
+
+    End Sub
+
+    Dim contador As Integer = 0
+
+    Private Sub cambiar_claves()
+
+
+
+        Dim rand As New Random()
+
+        Try
+            Dim clave_1 As Integer = rand.Next(100, 999999)
+            Dim clave_2 As Integer = rand.Next(200, 999999)
+            Dim clave_3 As Integer = rand.Next(300, 999999)
+            Dim clave_4 As Integer = rand.Next(400, 999999)
+            Dim clave_5 As Integer = rand.Next(500, 999999)
+            Dim clave_6 As Integer = rand.Next(600, 999999)
+            Dim clave_7 As Integer = rand.Next(700, 999999)
+            Dim clave_8 As Integer = rand.Next(800, 999999)
+            Dim clave_9 As Integer = rand.Next(900, 999999)
+            Dim clave_10 As Integer = rand.Next(1, 999999)
+
+
+            Me.LB_CLAVE_1.Text = get_string_clave(clave_1)
+            Me.LB_CLAVE_2.Text = get_string_clave(clave_2)
+            Me.LB_CLAVE_3.Text = get_string_clave(clave_3)
+            Me.LB_CLAVE_4.Text = get_string_clave(clave_4)
+            Me.LB_CLAVE_5.Text = get_string_clave(clave_5)
+            Me.LB_CLAVE_6.Text = get_string_clave(clave_6)
+            Me.LB_CLAVE_7.Text = get_string_clave(clave_7)
+            Me.LB_CLAVE_8.Text = get_string_clave(clave_8)
+            Me.LB_CLAVE_9.Text = get_string_clave(clave_9)
+            Me.LB_CLAVE_10.Text = get_string_clave(clave_10)
+
+
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Function get_string_clave(ByVal clave As Integer) As String
+        Dim str As String = ""
+
+        Select Case CType(clave.ToString.Length, Integer)
+            Case 1
+                str = "00000" + clave.ToString
+            Case 2
+                str = "0000" + clave.ToString
+            Case 3
+                str = "000" + clave.ToString
+            Case 4
+                str = "00" + clave.ToString
+            Case 5
+                str = "0" + clave.ToString
+            Case 6
+                str = clave.ToString
+        End Select
+
+        Return str
+
+
+    End Function
+
+    Private Sub TimerConteo_Tick(sender As Object, e As EventArgs) Handles TimerConteo.Tick
+
+        contador = contador + 1
+        Me.lbTimer.Text = contador
+        If contador = 60 Then contador = 0
+
+    End Sub
+End Class
